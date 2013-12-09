fd31bfabcb73228117120d0d19e3c9e15b91ee8e
nadia12 <nadia_riantini@ymail.com>
2013-12-09 13:22:21 +0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vouchr::Application.config.secret_key_base = '50da2fe1d6a7cccb850ac9a2c62151775f80c7ac412043d946589fe28813f5da5ae81432d3efd40538c926479e8fcb886e51f5097516028ad394ea9908f9d78a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Vouchr::Application.config.session_store :cookie_store, key: '_vouchr_session'
