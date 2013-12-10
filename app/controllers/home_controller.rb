7d1d2e4b07f99c6eb0015e762c3febfe6e48b263
izqil <ahmad.hizqil@gmail.com>
2013-12-10 15:54:54 +0700

    after ajax for index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,17 +5,21 @@ class HomeController < ApplicationController
 
   def new
     number = params[:phone_number]
+    @valid = Home.validate_prefix number
     respond_to do |format|
-      if Home.validate_prefix number
-        format.html { redirect_to home_index_path, notice: 'Your number is valid' }
+      if @valid
+        format.html { redirect_to home_index_path }
+        format.js
       else
-        format.html { redirect_to home_index_path , notice: 'Nomor anda tidak terdaftar' }
+        format.html { redirect_to home_index_path }
+        format.js
       end
     end
   end
 
   def show
     @vouchers = Voucher.all
+    render "_show"
   end
 
   def generate_luhn
